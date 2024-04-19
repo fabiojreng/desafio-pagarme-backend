157529857b1e5274eaef1b50c81f054162d57314
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-04-19 16:29:50 -0300

    feat: Create restore method and __str__ for transaction entity

--- src/Domain/Entities/transaction.py
+++ src/Domain/Entities/transaction.py
@@ -1,19 +1,26 @@
-from ..VO.payment_method import PaymentMethod
-from ..VO.card_number import CardNumber
-from ..VO.card_expiration import CardExpiration
+import uuid
+from src.Domain.Entities.client import Name
+from src.Domain.VO.Transaction.card_expiration import CardExpiration
+from src.Domain.VO.Transaction.card_number import CardNumber
+from src.Domain.VO.Transaction.cvv import CVV
+from src.Domain.VO.Transaction.payment_method import PaymentMethod
+from src.Domain.VO.Transaction.transaction_description import TransactionDescription
+from src.Domain.VO.Transaction.transaction_value import TransactionValue
 
 
 class Transaction:
     def __init__(
         self,
-        transaction_value: float,
-        transaction_description: str,
+        # id: str,
+        transaction_value: TransactionValue,
+        transaction_description: TransactionDescription,
         payment_method: PaymentMethod,
         card_number: CardNumber,
-        cardholder_name: str,
+        cardholder_name: Name,
         card_expiration: CardExpiration,
-        cvv: int,
+        cvv: CVV,
     ) -> None:
+        # self.__id = id
         self.__transaction_value = transaction_value
         self.__transaction_description = transaction_description
         self.__payment_method = payment_method
@@ -30,17 +37,46 @@ class Transaction:
         card_number: str,
         cardholder_name: str,
         card_expiration: str,
-        cvv: int,
+        cvv: str,
     ):
+        # id = uuid.uuid4()
         return Transaction(
-            transaction_value,
-            transaction_description,
+            # id,
+            TransactionValue(transaction_value),
+            TransactionDescription(transaction_description),
             PaymentMethod(payment_method),
             CardNumber(card_number),
-            cardholder_name,
+            Name(cardholder_name),
             CardExpiration(card_expiration),
-            cvv,
+            CVV(cvv),
         )
+
+    def restore(
+        # id: str,
+        transaction_value: float,
+        transaction_description: str,
+        payment_method: str,
+        card_number: str,
+        cardholder_name: str,
+        card_expiration: str,
+        cvv: str,
+    ):
+        return Transaction(
+            # id,
+            TransactionValue(transaction_value),
+            TransactionDescription(transaction_description),
+            PaymentMethod(payment_method),
+            CardNumber(card_number),
+            Name(cardholder_name),
+            CardExpiration(card_expiration),
+            CVV(cvv),
+        )
+
+    def __str__(self):
+        return f"Transaction Value: {self.__transaction_value}, Description: {self.__transaction_description}, Payment Method: {self.__payment_method}, Card Number: {self.__card_number}, Cardholder Name: {self.__cardholder_name}, Card Expiration: {self.__card_expiration}, CVV: {self.__cvv}"
+
+    def get_id(self):
+        return self.__id
 
     def get_transaction_value(self):
         return self.__transaction_value
