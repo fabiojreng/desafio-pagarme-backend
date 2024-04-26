a328496a417278a09430506fe6887787fe0a80f7
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-04-26 19:21:21 -0300

    feat: Register new routes

--- src/Infra/Controller/main_controller.py
+++ src/Infra/Controller/main_controller.py
@@ -8,15 +8,23 @@ class MainController:
         http_server: HttpServer,
         create_transaction: UseCaseInterface,
         create_client: UseCaseInterface,
+        find_all_transactions: UseCaseInterface,
+        client_saldo=UseCaseInterface,
     ) -> None:
         self.__create_transaction_use_case = create_transaction
         self.__create_client_use_case = create_client
+        self.__find_all_transactions = find_all_transactions
+        self.__client_saldo = client_saldo
         http_server.register("get", "/", self.__server)
-        http_server.register("post", "/transaction", self.__create_transaction)
-        http_server.register("post", "/client", self.__create_client)
+        http_server.register("post", "/create-transaction", self.__create_transaction)
+        http_server.register("post", "/create-client", self.__create_client)
+        http_server.register("get", "/transactions", self.__transactions)
+        http_server.register(
+            "get", "/client-amount/<client_id>/<status>", self.__calculate_saldo
+        )
 
-    def __server(self):
-        return {"statusCode": 200, "body": {"message": "Bem Vindo"}}
+    def __server(self, req):
+        return {"status_code": 200, "body": {"message": "Bem Vindo"}}
 
     def __create_transaction(self, req):
         output = self.__create_transaction_use_case.execute(req)
@@ -24,4 +32,12 @@ class MainController:
 
     def __create_client(self, req):
         output = self.__create_client_use_case.execute(req)
+        return output
+
+    def __transactions(self):
+        output = self.__find_all_transactions.execute()
+        return output
+
+    def __calculate_saldo(self, req):
+        output = self.__client_saldo.execute(req)
         return output
