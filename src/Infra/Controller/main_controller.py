6ca552951909af606f1d332e9680a3aa962333a3
Fábio Júnior <fabiojunioreloi@hotmail.com>
2024-04-27 13:00:50 -0300

    feat: Create the route to return payment by id

--- src/Infra/Controller/main_controller.py
+++ src/Infra/Controller/main_controller.py
@@ -9,12 +9,14 @@ class MainController:
         create_transaction: UseCaseInterface,
         create_client: UseCaseInterface,
         find_all_transactions: UseCaseInterface,
-        client_saldo=UseCaseInterface,
+        client_saldo: UseCaseInterface,
+        get_payable_id: UseCaseInterface,
     ) -> None:
         self.__create_transaction_use_case = create_transaction
         self.__create_client_use_case = create_client
         self.__find_all_transactions = find_all_transactions
         self.__client_saldo = client_saldo
+        self.__get_payable_by_id = get_payable_id
         http_server.register("get", "/", self.__server)
         http_server.register("post", "/create-transaction", self.__create_transaction)
         http_server.register("post", "/create-client", self.__create_client)
@@ -22,8 +24,9 @@ class MainController:
         http_server.register(
             "get", "/client-amount/<client_id>/<status>", self.__calculate_saldo
         )
+        http_server.register("get", "/payable/<payable_id>", self.get_payable_by_id)
 
-    def __server(self, req):
+    def __server(self):
         return {"status_code": 200, "body": {"message": "Bem Vindo"}}
 
     def __create_transaction(self, req):
@@ -34,10 +37,14 @@ class MainController:
         output = self.__create_client_use_case.execute(req)
         return output
 
-    def __transactions(self):
-        output = self.__find_all_transactions.execute()
+    def __transactions(self, req):
+        output = self.__find_all_transactions.execute(req)
         return output
 
     def __calculate_saldo(self, req):
         output = self.__client_saldo.execute(req)
+        return output
+
+    def get_payable_by_id(self, req):
+        output = self.__get_payable_by_id.execute(req)
         return output
